b517fe3c19a90377a65f18c30daa5ebe358dd66e
Mario Halambek <mario.halambek@sartura.hr>
2015-01-07 10:41:25 +0000

    example: changed datastore root to use define in header
    Signed-off-by: Mario Halambek <mario.halambek@sartura.hr>

--- examples/sandwich.c
+++ examples/sandwich.c
@@ -23,7 +23,7 @@ __unused void destroy();
 struct module m;
 char *ns = "urn:ietf:params:xml:ns:yang:sandwich";
 
-datastore_t root = {"root",NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,0,0};
+datastore_t root = DATASTORE_ROOT_DEFAULT;
 
 static int set_name(char *name)
 {
